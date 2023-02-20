d50de4f07a1caf6a2cad4d0dcef7c96ab236fb0c
andrewaucie <aucieandrew@gmail.com>
2023-02-20 17:15:32 -0500

    profile schema changes

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+require('./db');
+require('./schemas/Profile');
 const express = require ('express');
+const bodyParser = require('body-parser')
 const port = 3000;
-const app = express
+const app = express();
+const profileRoutes = require('./routes/profileRoutes');
+//app.use(bodyParser.urlencoded({
+   // extended: true
+//}));
+app.use(bodyParser.json());
 
+app.use(profileRoutes);
 
-require('./db')
+//requireToken skipped
+
+app.get('/', (req, res)=>{
+    res.send("hello3s world");
+})
+
+app.listen(port, ()=>{
+    console.log("Server is running on port " + port);
+})
