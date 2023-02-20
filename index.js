77cef4230c49ac317d4f16574599089a046f6fbb
Adam Badar <badaradam10@gmail.com>
2023-02-20 15:14:34 -0500

    modified:   .env 	modified:   index.js 	modified:   node_modules/.bin/color-support 	modified:   node_modules/.bin/fxparser 	modified:   node_modules/.bin/mime 	modified:   node_modules/.bin/mkdirp 	modified:   node_modules/.bin/node-pre-gyp 	modified:   node_modules/.bin/nodemon 	modified:   node_modules/.bin/nodetouch 	modified:   node_modules/.bin/nopt 	modified:   node_modules/.bin/rimraf 	modified:   node_modules/.bin/semver 	modified:   node_modules/.bin/uuid 	modified:   node_modules/.package-lock.json 	modified:   node_modules/jsonwebtoken/node_modules/.bin/semver 	modified:   node_modules/make-dir/node_modules/.bin/semver 	modified:   node_modules/nodemon/node_modules/.bin/semver 	modified:   node_modules/simple-update-notifier/node_modules/.bin/semver 	modified:   node_modules/touch/node_modules/.bin/nopt 	modified:   package-lock.json 	modified:   package.json
    	models/
    	node_modules/.bin/color-support.cmd
    	node_modules/.bin/color-support.ps1
    	node_modules/.bin/fxparser.cmd
    	node_modules/.bin/fxparser.ps1
    	node_modules/.bin/mime.cmd
    	node_modules/.bin/mime.ps1
    	node_modules/.bin/mkdirp.cmd
    	node_modules/.bin/mkdirp.ps1
    	node_modules/.bin/node-pre-gyp.cmd
    	node_modules/.bin/node-pre-gyp.ps1
    	node_modules/.bin/nodemon.cmd
    	node_modules/.bin/nodemon.ps1
    	node_modules/.bin/nodetouch.cmd
    	node_modules/.bin/nodetouch.ps1
    	node_modules/.bin/nopt.cmd
    	node_modules/.bin/nopt.ps1
    	node_modules/.bin/rimraf.cmd
    	node_modules/.bin/rimraf.ps1
    	node_modules/.bin/semver.cmd
    	node_modules/.bin/semver.ps1
    	node_modules/.bin/uuid.cmd
    	node_modules/.bin/uuid.ps1
    	node_modules/@socket.io/
    	node_modules/@types/cookie/
    	node_modules/@types/cors/
    	node_modules/base64id/
    	node_modules/cors/
    	node_modules/engine.io-parser/
    	node_modules/engine.io/
    	node_modules/jsonwebtoken/node_modules/.bin/semver.cmd
    	node_modules/jsonwebtoken/node_modules/.bin/semver.ps1
    	node_modules/make-dir/node_modules/.bin/semver.cmd
    	node_modules/make-dir/node_modules/.bin/semver.ps1
    	node_modules/nodemon/node_modules/.bin/semver.cmd
    	node_modules/nodemon/node_modules/.bin/semver.ps1
    	node_modules/simple-update-notifier/node_modules/.bin/semver.cmd
    	node_modules/simple-update-notifier/node_modules/.bin/semver.ps1
    	node_modules/socket.io-adapter/
    	node_modules/socket.io-parser/
    	node_modules/socket.io/
    	node_modules/touch/node_modules/.bin/nopt.cmd
    	node_modules/touch/node_modules/.bin/nopt.ps1
    	node_modules/ws/
    	routes/
    	schemas/GroupChat.js
    	schemas/Message.js

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
-const express = require ('express');
+const express = require('express');
 const port = 3000;
-const app = express
+const app = express();
+const bodyParser = require('body-parser');
+require('./db');
+require('./schemas/GroupChat');
+require('./schemas/Message');
+const authRoutes = require('./routes/AuthRoutes');
+const uploadMediaRoutes = require('./routes/SendFiles');
+const messageRoutes = require('./routes/MessageRoutes');
+//requireToken skipped
 
 
-require('./db')
+//.......18
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+
+
+const io = new Server(httpServer, { /* options */ });
+
+
+//
+
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(uploadMediaRoutes);
+app.use(messageRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World");
+// })
+
+
+//...........18
+
+io.on("connection", (socket) => {
+
+    console.log("USER CONNECTED - ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED - ", socket.id);
+    });
+
+    socket.on("join_room", (data) => {
+        console.log("USER WITH ID - ",socket.id,"JOIN ROOM - ", data.roomid);
+        socket.join(data);
+    });
+
+    socket.on("send_message", (data) => {
+        console.log("MESSAGE RECEIVED - ", data);
+        io.emit("receive_message", data);
+    });
+});
+
+
+httpServer.listen(3001);
+
+// ...
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+})
+
+
+
+// 6353f0c6a52cde6dafae64d2634c1d60f09a4f3ff40be517
+// 6353f0c6a52cde6dafae64d2634c1d60f09a4f3ff40be517
